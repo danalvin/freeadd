9c11d3e2f7228eeb3f81d8f4ebe1be7b4c1f6d46
danalvin <mungaiwaituika@gmail\.com>
2021-09-03 07:23:05 +0300

    Third sept changes

--- users/models.py
+++ users/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.dispatch import receiver
+from django.db.models.signals import pre_save
 
 
 # Create your models here.
@@ -28,3 +30,12 @@ class User(AbstractUser):
         return self.username
 
 
+@receiver(pre_save, sender=User)
+def update_username_from_email(sender, instance, **kwargs):
+    user_email = instance.email
+    username = user_email[:30]
+    n = 1
+    while User.objects.exclude(pk=instance.pk).filter(username=username).exists():
+        n += 1
+        username = user_email[:(29 - len(str(n)))] + '-' + str(n)
+    instance.username = username
