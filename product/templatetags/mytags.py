8053aa5d64c2f7ecb08e8eaf55445ce82a5a3d49
danalvin <mungaiwaituika@gmail\.com>
2021-09-29 13:31:57 +0300

    Categories min

--- product/templatetags/mytags.py
+++ product/templatetags/mytags.py
@@ -10,9 +10,9 @@ def addclass(value, arg):
 
 @register.inclusion_tag('_categorieslist.html')
 def show_categories():
-    categories = Category.objects.annotate(product_count=Count("product")).filter(product_count__gt=0).order_by('-product_count','name') 
+    # categories = Category.objects.annotate(product_count=Count("product")).filter(product_count__gt=0).order_by('-product_count','name') 
     #categories = Category.objects.annotate(post_count=Count("blog_posts")).filter(post_count__gt=0).order_by('name')
-    # categories = Category.objects.all().order_by('name')
+    categories = Category.objects.all().order_by('name')
     return{'categories': categories}
 
 @register.inclusion_tag('_categoriessidelist.html')
